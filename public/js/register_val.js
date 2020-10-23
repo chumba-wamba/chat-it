c13954000f32d83013930422212cb312288a3a3c
chumba-wamba <nisarg0271@gmail.com>
2020-10-23 21:14:54 +0530

    Added basic client-side form validation viz js (& HTML fields) :zap:

--- public/js/register_val.js
+++ public/js/register_val.js
@@ -6,13 +6,19 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   let messages = {
-    lengthError: "length must be between 6 and 20 characters",
+    userNameError: "length must be between 6 and 20 characters",
+    passwordError: "password is too small",
   };
 
   isFaulty = false;
   if (userName.value.length < 6 || userName.value.length >= 20) {
     isFaulty = true;
-    document.getElementById("userNameError").innerText = messages.lengthError;
+    document.getElementById("userNameError").innerText = messages.userNameError;
+  }
+
+  if (password.value.length < 6) {
+    isFaulty = true;
+    document.getElementById("passwordError").innerText = messages.passwordError;
   }
 
   if (isFaulty) {
